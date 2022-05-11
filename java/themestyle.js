05e133a547de7322fa56ddcede7491a643efc0fe
yasir-72888 <105175453+yasir-72888@users.noreply.github.com>
2022-05-11 20:28:35 +0200

    yasir-add3

---
+++ java/themestyle.js
@@ -0,0 +1,13 @@
+var head = document.querySelectorAll("head")[0];
+var link = document.createElement("link");
+
+link.rel = "stylesheet";
+link.type = "text/css";
+
+if (theme_key==1){
+    link.href = "css/style.min.css";
+}
+else if(theme_key==2){
+    link.href = "css/style2.min.css";
+}
+head.appendChild(link);
